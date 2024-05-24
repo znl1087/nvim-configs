0dced12b904d9bd2060150cf53473a6ed67907d7
znl <lovecjnforever@gmail.com>
2024-05-25 00:50:53 +0800

    add no wrap

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,9 @@
 -- Hint: use `:h <option>` to figure out the meaning if needed
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.opt.completeopt = {"menu", "menuone", "noselect"}
 vim.opt.mouse = "a" -- allow the mouse to be used in Nvim
 
@@ -23,3 +27,4 @@ vim.opt.incsearch = true -- search as characters are entered
 vim.opt.hlsearch = false -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
 vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+vim.opt.wrap = false -- do not wrap around the end of the file
