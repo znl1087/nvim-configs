2ea8295ff458f5ef6492162b1f400da47f95d928
znl <lovecjnforever@gmail.com>
2024-05-25 00:51:50 +0800

    add no wrap

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,5 @@
 -- Hint: use `:h <option>` to figure out the meaning if needed
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.opt.completeopt = {"menu", "menuone", "noselect"}
 vim.opt.mouse = "a" -- allow the mouse to be used in Nvim
 
