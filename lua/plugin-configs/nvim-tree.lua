f270be82879b3faa96e4cf9fec8c4a179ad91b56
znl <lovecjnforever@gmail.com>
2024-05-27 01:45:01 +0800

    nvim tree config optimize

--- lua/plugin-configs/nvim-tree.lua
+++ lua/plugin-configs/nvim-tree.lua
@@ -28,7 +28,10 @@ require("nvim-tree").setup(
     respect_buf_cwd = true,
     update_focused_file = {
       enable = true,
-      update_root = true 
+      update_root = true
+    },
+    git = {
+      timeout = 5000
     }
   }
 )
