36969ff7ba811b32bcf0edba477a3162cc23476c
znl1087 <lovecjnforever@gmail.com>
2024-06-04 19:15:59 +0800

    python init config

--- lua/plugin-configs/formatter-nvim.lua
+++ lua/plugin-configs/formatter-nvim.lua
@@ -7,6 +7,7 @@ local status, formatter = pcall(require, "formatter")
 if (not status) then
     return
 end
+local util = require "formatter.util"
 formatter.setup {
     filetype = {
         lua = {
@@ -14,6 +15,17 @@ formatter.setup {
                 return {
                     exe = "luafmt",
                     args = { "--indent-count", 2, "--stdin" },
+                    stdin = true
+                }
+            end
+        },
+        python = {
+            function()
+                return {
+                    exe = "autopep8",
+                    args = {
+                        util.escape_path(util.get_current_buffer_file_path())
+                    },
                     stdin = true
                 }
             end
