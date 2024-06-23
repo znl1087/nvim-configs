7104fc3af9cad359d5e3069cfff3da3d5f747972
znl <lovecjnforever@gmail.com>
2024-06-23 23:03:14 +0800

    auto intent = true

--- lua/options.lua
+++ lua/options.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop = 4      -- number of visual spaces per TAB
 vim.opt.softtabstop = 4  -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4   -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+vim.opt.autoindent = true -- copy indent from current line when starting a new line
 
 -- UI config
 vim.opt.number = true         -- show absolute number
